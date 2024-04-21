793a8b7a0cbb05fb11672b5266be8aaf58753f86
matheus-dev-fullstack <matheus178541@gmail.com>
2024-04-21 20:36:46 -0300

    Configurando Models e ajustando Admin para gerenciar esses Models

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from myapp import models
+from myapp.models import Client, Immobile, ImmobileImage, RegisterLocation
 
-# Register your models here.
+# admin.site.register(Immobile)
+# admin.site.register(ImmobileImage)
+
+admin.site.register(Client)
+admin.site.register(RegisterLocation)
+
+class ImmobileImageInlineAdmin(admin.TabularInline):
+    model = models.ImmobileImage
+    extra = 0
+    
+class ImmobileAdmin(admin.ModelAdmin):
+    inlines = [ImmobileImageInlineAdmin]
+    
+admin.site.register(models.Immobile, ImmobileAdmin)
